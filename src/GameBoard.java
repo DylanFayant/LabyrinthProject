8362284854971146ae9799698806aee0803a1a17
DylanFayant <dylan.fayant@iut-valence.fr>
2016-04-08 13:26:51 +0200

    List of tiles defined here

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import Exceptions.*;
 
 /**
@@ -7,8 +9,13 @@ import Exceptions.*;
  */
 public class GameBoard {
 
+	// TODO write comment
 	private static final int WIDTH = 7;
+	
+	// TODO write comment
 	private static final int HEIGHT = 7;
+	
+	// TODO write comment
 	TilePositioned[][] gameBoard;
 	
 	/**
@@ -17,26 +24,34 @@ public class GameBoard {
 	public GameBoard() {
 		gameBoard = new TilePositioned[GameBoard.WIDTH][GameBoard.HEIGHT];
 		
-		// TODO: finish the generation of the fixed tiles
+		ArrayList tilesAvailable = new ArrayList();
+		
+		Tile[] listTiles = Tile.values();		
+		for(Tile aTile : listTiles)
+		{
+			//tilesAvailable()
+		}
+		
+		// generation of the fixed tiles
 		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
-		gameBoard[0][0] = new TilePositioned(Tile.TILE3, Rotation.ROTATION2, true);
-		gameBoard[0][0] = new TilePositioned(Tile.TILE5, Rotation.ROTATION2, true);
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
+		gameBoard[2][0] = new TilePositioned(Tile.TILE3, Rotation.ROTATION1, true);
+		gameBoard[4][0] = new TilePositioned(Tile.TILE5, Rotation.ROTATION1, true);
+		gameBoard[6][0] = new TilePositioned(Tile.TILE7, Rotation.ROTATION3, true);
 
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
+		gameBoard[0][2] = new TilePositioned(Tile.TILE15, Rotation.ROTATION4, true);
+		gameBoard[2][2] = new TilePositioned(Tile.TILE17, Rotation.ROTATION4, true);
+		gameBoard[2][2] = new TilePositioned(Tile.TILE19, Rotation.ROTATION1, true);
+		gameBoard[6][2] = new TilePositioned(Tile.TILE21, Rotation.ROTATION2, true);
 
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
+		gameBoard[0][4] = new TilePositioned(Tile.TILE29, Rotation.ROTATION4, true);
+		gameBoard[2][4] = new TilePositioned(Tile.TILE31, Rotation.ROTATION3, true);
+		gameBoard[4][4] = new TilePositioned(Tile.TILE33, Rotation.ROTATION2, true);
+		gameBoard[6][4] = new TilePositioned(Tile.TILE35, Rotation.ROTATION2, true);
 
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
-		gameBoard[0][0] = new TilePositioned(Tile.TILE1, Rotation.ROTATION2, true);
+		gameBoard[0][6] = new TilePositioned(Tile.TILE43, Rotation.ROTATION1, true);
+		gameBoard[2][6] = new TilePositioned(Tile.TILE45, Rotation.ROTATION3, true);
+		gameBoard[4][6] = new TilePositioned(Tile.TILE47, Rotation.ROTATION3, true);
+		gameBoard[6][6] = new TilePositioned(Tile.TILE49, Rotation.ROTATION4, true);
 	}
 
 	/**
